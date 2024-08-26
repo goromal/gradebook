f1e667847286580e2c2b96c214cf6a35630a7ec9
Andrew Torgesen <atorgesen@anduril.com>
2024-08-25 19:28:46 -0700

    basic tests done

--- gradebook/cli.py
+++ gradebook/cli.py
@@ -1,5 +1,120 @@
+import click
+from colorama import Fore, Style
+import grpc
+from google.protobuf.timestamp_pb2 import Timestamp
+
+from aapis.gradebook.v1 import gradebook_pb2_grpc, gradebook_pb2
+
+
+DEFAULT_INSECURE_PORT = 40080
+
+
+@click.group()
+@click.pass_context
+@click.option(
+    "--address",
+    type=str,
+    default="localhost",
+    show_default=True,
+    help="Address of the gradebookd server.",
+)
+@click.option(
+    "--port",
+    type=int,
+    default=DEFAULT_INSECURE_PORT,
+)
+def cli(ctx: click.Context, address, port):
+    """Manage your database of software requirements."""
+    ctx.obj = {"address": address, "insecure_port": port}
+
+
+@cli.group()
+@click.pass_context
+def add(ctx: click.Context):
+    """Add to the database."""
+
+
+@add.command()
+@click.pass_context
+@click.argument("id")
+@click.argument("tag")
+@click.argument("text")
+@click.option(
+    "-p",
+    "--parent",
+    type=str,
+    default="",
+    help="Optional specification of a parent tag for nested requirements.",
+)
+def requirement(ctx: click.Context, id, tag, text, parent):
+    with grpc.insecure_channel(
+        f"{ctx.obj['address']}:{ctx.obj['insecure_port']}"
+    ) as channel:
+        stub = gradebook_pb2_grpc.GradebookServiceStub(channel)
+        print(Fore.YELLOW + "Adding requirement:" + Style.RESET_ALL)
+        print(f"  Unique ID:      {id}")
+        print(f"  Searchable Tag: {parent}/{tag}")
+        print(f"  Text: {text}")
+        response = stub.CreateRequirement(
+            gradebook_pb2.CreateRequirementRequest(
+                requirement=gradebook_pb2.Requirement(
+                    req_uuid=id,
+                    tag=tag,
+                    parent_tag=tag,
+                    status=gradebook_pb2.RequirementStatus.REQUIREMENT_STATUS_ACTIVE,
+                    text=text,
+                )
+            )
+        )
+        if response.success:
+            print(Fore.GREEN + "SUCCESS" + Style.RESET_ALL)
+        else:
+            print(Fore.RED + "FAILED:" + Style.RESET_ALL + response.message)
+
+
+@add.command()
+@click.pass_context
+@click.argument("id")
+@click.argument("unit")
+@click.argument("hash")
+@click.argument("passed")
+def result(ctx: click.Context, id, unit, hash, passed):
+    test_date = Timestamp()
+    test_date.GetCurrentTime()
+    if passed in [1, "1", True, "True", "true", "t", "T"]:
+        passed = True
+    else:
+        passed = False
+    with grpc.insecure_channel(
+        f"{ctx.obj['address']}:{ctx.obj['insecure_port']}"
+    ) as channel:
+        stub = gradebook_pb2_grpc.GradebookServiceStub(channel)
+        print(Fore.YELLOW + "Adding test result:" + Style.RESET_ALL)
+        print(f"  Unique ID:       {id}")
+        print(f"  Unit Under Test: {unit}")
+        print(f"  Software Hash:   {hash}")
+        print(f"  Test Date:       {test_date}")
+        print(f"  Passed:          {passed}")
+        response = stub.SubmitTestResult(
+            gradebook_pb2.SubmitTestResultRequest(
+                result=gradebook_pb2.TestResult(
+                    req_uuid=id,
+                    test_unit=unit,
+                    software_hash=hash,
+                    test_date=test_date,
+                    passed=passed,
+                )
+            )
+        )
+        if response.success:
+            print(Fore.GREEN + "SUCCESS" + Style.RESET_ALL)
+        else:
+            print(Fore.RED + "FAILED:" + Style.RESET_ALL + response.message)
+
+
 def main():
-    print("Hello, world!")
+    cli()
+
 
 if __name__ == "__main__":
     main()
--- gradebook/daemon.py
+++ gradebook/daemon.py
@@ -12,6 +12,7 @@ from gradebook.click_types import LogLevel
 
 DEFAULT_INSECURE_PORT = 40080
 
+
 def _get_req_status_string(status):
     if status == gradebook_pb2.RequirementStatus.REQUIREMENT_STATUS_UNSPECIFIED:
         return "UNSPECIFIED"
@@ -27,15 +28,23 @@ def _get_req_status_string(status):
 
 class Gradebook(gradebook_pb2_grpc.GradebookServiceServicer):
     def __init__(self, database_file):
-        self.db_path = database_file
+        self.db_path = os.path.expanduser(database_file)
         if not os.path.exists(self.db_path):
             self._create_database()
-    
+
     def _create_database(self):
         conn = sqlite3.connect(self.db_path)
         conn.close()
 
     def CreateRequirement(self, request, context):
+        insertion_data = [
+            request.requirement.req_uuid,
+            request.requirement.tag,
+            request.requirement.parent_tag,
+            _get_req_status_string(request.requirement.status),
+            request.requirement.text,
+        ]
+        logging.info(f"Received request to insert requirement {insertion_data}")
         table_name = "Requirements"
 
         # Open a connection to the SQLite database
@@ -51,32 +60,37 @@ class Gradebook(gradebook_pb2_grpc.GradebookServiceServicer):
         columns = "req_uuid, tag, parent_tag, status, text"
         placeholders = "?, ?, ?, ?, ?"
         insert_query = f"INSERT INTO {table_name} ({columns}) VALUES ({placeholders});"
-        
+
         try:
-            cursor.execute(insert_query, [
-                request.req_uuid,
-                request.tag,
-                request.parent_tag,
-                _get_req_status_string(request.status),
-                request.text,
-            ])
+            cursor.execute(
+                insert_query,
+                insertion_data,
+            )
             conn.commit()
+            logging.info("Success")
             response = gradebook_pb2.CreateRequirementResponse(
-                message=f"Entry added to {table_name}.",
-                success=True
+                message=f"Entry added to {table_name}.", success=True
             )
         except sqlite3.Error as e:
+            logging.error(f"Failed: {e}")
             response = gradebook_pb2.CreateRequirementResponse(
-                message=str(e),
-                success=False
+                message=str(e), success=False
             )
         finally:
             cursor.close()
             conn.close()
 
         return response
-    
+
     def SubmitTestResult(self, request, context):
+        insertion_data = [
+            request.result.req_uuid,
+            request.result.test_date.ToSeconds(),
+            request.result.test_unit,
+            request.result.software_hash,
+            request.result.passed,
+        ]
+        logging.info(f"Received request to insert test result {insertion_data}")
         table_name = "Results"
 
         # Open a connection to the SQLite database
@@ -94,36 +108,34 @@ class Gradebook(gradebook_pb2_grpc.GradebookServiceServicer):
         insert_query = f"INSERT INTO {table_name} ({columns}) VALUES ({placeholders});"
 
         try:
-            cursor.execute(insert_query, [
-                request.req_uuid,
-                request.test_data.ToSeconds(),
-                request.test_unit,
-                request.software_hash,
-                request.passed,
-            ])
+            cursor.execute(insert_query, insertion_data)
             conn.commit()
+            logging.info("Success")
             response = gradebook_pb2.SubmitTestResultResponse(
-                message=f"Entry added to {table_name}.",
-                success=True
+                message=f"Entry added to {table_name}.", success=True
             )
         except sqlite3.Error as e:
+            logging.error(f"Failed: {e}")
             response = gradebook_pb2.SubmitTestResultResponse(
-                message=str(e),
-                success=False
+                message=str(e), success=False
             )
         finally:
             cursor.close()
             conn.close()
-        
+
         return response
+
 
 def serve(port, database_file):
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    gradebook_pb2_grpc.add_GradebookServiceServicer_to_server(Gradebook(database_file), server)
+    gradebook_pb2_grpc.add_GradebookServiceServicer_to_server(
+        Gradebook(database_file), server
+    )
     server.add_insecure_port(f"[::]:{port}")
-    print(f"Starting gradebook server on port {port}")
+    logging.info(f"Starting gradebook server on port {port}")
     server.start()
     server.wait_for_termination()
+
 
 @click.command()
 @click.option(
@@ -132,11 +144,7 @@ def serve(port, database_file):
     type=int,
     default=DEFAULT_INSECURE_PORT,
 )
-@click.option(
-    "--database-file",
-    type=click.Path(),
-    default="~/"
-)
+@click.option("--database-file", type=click.Path(), default="~/gradebook.db")
 @click.option(
     "-l",
     "--log-level",
@@ -149,5 +157,10 @@ def cli(port, database_file, log_level):
     logging.info(f"Log level set to {log_level}")
     serve(port, database_file)
 
-if __name__ == '__main__':
+
+def main():
     cli()
+
+
+if __name__ == "__main__":
+    main()
